8653d740113b02f812bd58789f147caadae4d753
chiragtaneja915 <tchirag915@gmail.com>
2021-12-17 02:48:02 +0530

    modify settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -136,6 +136,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_ROOT = BASE_DIR / 'static/images/'
+MEDIA_URL = '/images/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
